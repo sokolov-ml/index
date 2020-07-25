208fc8c2279620bb083b2d96c51876c539b838b9
Mikahil Sokolov <pilligrim17@yandex.ru>
2020-07-25 17:08:10 +0300

    add logging

--- scripts/index.js
+++ scripts/index.js
@@ -5,5 +5,32 @@ square.addEventListener("click", () => {
 });
 
 document.addEventListener("keydown", (event) => {
-  square.textContent = `keyCode: ${event.keyCode} (${event.key})`;
+  const logText = `keyCode: ${event.keyCode} (${event.key})`;
+
+  const log = new Log(logText);
+  square.append(log.createLine());
 });
+
+class Log {
+  constructor(text) {
+    this._time = new Date();
+    this._text = text;
+    this._template = document.querySelector("#log").content.cloneNode(true);
+  }
+
+  createLine() {
+    let time =
+      String(this._time.getHours()).padStart(2, "0") +
+      ":" +
+      String(this._time.getMinutes()).padStart(2, "0") +
+      ":" +
+      String(this._time.getSeconds()).padStart(2, "0");
+
+    this._template.querySelector(".log__time").textContent = time;
+    this._template.querySelector(".log__text").textContent = this._text;
+
+    console.log(this._template);
+
+    return this._template;
+  }
+}
